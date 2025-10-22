871eb269ef2cb92ddf221c64760aedefd4907c59
Jacob <schmidtjacob920@gmail.com>
2025-10-22 18:21:50 -0400

    Work on clipboard copy button

--- BorderRadiusPreviewer/script.js
+++ BorderRadiusPreviewer/script.js
@@ -1,21 +1,36 @@
 const box = document.getElementById("border-radius-display");
-const topLeftInput = document.getElementByName("top-left");
-const topRightInput = document.getElementByName("top-right");
-const bottomRightInput = document.getElementByName("bottom-right");
-const bottomLeftInput = document.getElementByName("bottom-left");
+const topLeftInput = document.getElementById("top-left");
+const topRightInput = document.getElementById("top-right");
+const bottomRightInput = document.getElementById("bottom-right");
+const bottomLeftInput = document.getElementById("bottom-left");
+const copyButton = document.getElementById("copy-button");
 
 
 
+topLeftInput.addEventListener('input', function() {
+	box.style.borderTopLeftRadius = `${topLeftInput.value}%`;
+});
 
-	topLeftInput.addEventListener('input', function() {
-		box.style.borderTopLeftRadius = `${topLeftInput.value}%`;
-	});
-	topRightInput.addEventListener('input', function() {
-		box.style.borderTopRightRadius = `${topRightInput.value}%`;
-	});
-	bottomRightInput.addEventListener('input', function() {
-		box.style.borderBottomRightRadius = `${bottomRightInput.value}%`;
-	});
-	bottomLeftInput.addEventListener('input', function() {
-		box.style.borderBottomLeftRadius = `${bottomLeftInput.value}%`;
-	});
+topRightInput.addEventListener('input', function() {
+	box.style.borderTopRightRadius = `${topRightInput.value}%`;
+});
+
+bottomRightInput.addEventListener('input', function() {
+	box.style.borderBottomRightRadius = `${bottomRightInput.value}%`;
+});
+
+bottomLeftInput.addEventListener('input', function() {
+	box.style.borderBottomLeftRadius = `${bottomLeftInput.value}%`;
+});
+
+copyButton.addEventListener('onclick', () => writeClipboardText("hello!"));
+
+
+
+async function writeClipboardText(text) {
+  try {
+    await navigator.clipboard.writeText(text);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
